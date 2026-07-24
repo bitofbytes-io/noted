fec72f89cfaa81b06a0a82a13b8894bebc0e48c9
Daniel Waters <danwater1@gmail.com>
2026-07-24 01:16:41 -0400

    Allow for CI browser startup latency

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -3,6 +3,9 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   timeout: 30_000,
+  expect: {
+    timeout: 15_000,
+  },
   use: {
     baseURL: 'http://127.0.0.1:4200',
     trace: 'retain-on-failure',
